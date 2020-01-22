8aa3b53bddb23f33b4071c8353fc8b1f67214698
kbh <nine4409@u.sogang.ac.kr>
2020-01-22 15:12:07 +0900

    dependency

---
+++ install_dependency.sh
@@ -0,0 +1,12 @@
+# https://webnautes.tistory.com/1030
+
+# install dependency
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev
+sudo apt-get -y install libv4l-dev v4l-utils
+sudo apt-get -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get -y install libqt4-dev
+sudo apt-get -y install mesa-utils libgl1-mesa-dri libqt4-opengl-dev
+sudo apt-get -y install libatlas-base-dev gfortran libeigen3-dev
+sudo apt-get -y install libgtk-3-dev
+
